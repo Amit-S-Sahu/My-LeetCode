11a7890b8677696e0ccb020a6e8bfb1b9d70f0fe
Amit S Sahu <Amit.123854@stu.upes.ac.in>
2024-09-30 16:19:29 +0530

    Time: 6 ms (52.36%), Space: 43.4 MB (33.54%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+        String[] strNums = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            strNums[i] = String.valueOf(nums[i]);
+        }
+
+        Arrays.sort(strNums, (a, b) -> (b + a).compareTo(a + b));
+
+        if (strNums[0].equals("0")) {
+            return "0";
+        }
+
+        StringBuilder ans = new StringBuilder();
+        for (String num : strNums) {
+            ans.append(num);
+        }
+
+        return ans.toString();
+    }
+}
