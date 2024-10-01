74483cc37adfcf4a00170b50987899c891927f7e
Amit S Sahu <Amit.123854@stu.upes.ac.in>
2024-10-01 10:20:56 +0530

    Time: 1 ms (84.5%), Space: 41.5 MB (86.64%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+
+        StringBuilder ans = new StringBuilder("");
+        int i = 0, j = 0;
+        while (i < n || j < m) {
+            if (i < n) ans.append(word1.charAt(i++));
+            if (j < m) ans.append(word2.charAt(j++));
+        }
+
+        return ans.toString();
+    }
+}
