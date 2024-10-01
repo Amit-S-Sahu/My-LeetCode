912b1cb4d5db819b26868eba027f382f497b9afb
Amit S Sahu <Amit.123854@stu.upes.ac.in>
2024-10-01 10:07:07 +0530

    Time: 23 ms (61.42%), Space: 45.3 MB (64.89%) - LeetHub

---
+++ 0729-my-calendar-i/0729-my-calendar-i.java
@@ -0,0 +1,24 @@
+class MyCalendar {
+    TreeMap<Integer, Integer> map;
+
+    public MyCalendar() {
+        map = new TreeMap<>();
+    }
+    
+    public boolean book(int start, int end) {
+        Integer prev = map.floorKey(start);
+        Integer next = map.ceilingKey(start);
+
+        if ((prev == null || map.get(prev) <= start) && (next == null || end <= next)) {
+            map.put(start, end);
+            return true;
+        }
+        return false;
+    }
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(start,end);
+ */
