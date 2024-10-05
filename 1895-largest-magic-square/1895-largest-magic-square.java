3a99afb7461285e055848042a53fee739112c649
Amit S Sahu <Amit.123854@stu.upes.ac.in>
2024-10-06 03:40:15 +0530

    Time: 21 ms (50.89%), Space: 44.3 MB (56.23%) - LeetHub

---
+++ 1895-largest-magic-square/1895-largest-magic-square.java
@@ -0,0 +1,64 @@
+class Solution {
+    public int largestMagicSquare(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        int min = Math.min(n, m); 
+        int ans = 1; 
+
+        for (int k = min; k > 1; k--) { 
+            for (int i = 0; i <= n - k; i++) { 
+                for (int j = 0; j <= m - k; j++) {
+                    boolean isMagic = true;
+                    int first_row = 0;
+                    for (int l = 0; l < k; l++) {
+                        first_row += grid[i][j + l];
+                    }
+
+                    for (int r = 0; r < k; r++) {
+                        int sum_row = 0;
+                        for (int c = 0; c < k; c++) {
+                            sum_row += grid[i + r][j + c];
+                        }
+                        if (sum_row != first_row) {
+                            isMagic = false;
+                            break;
+                        }
+                    }
+                    
+                    for (int c = 0; c < k && isMagic; c++) {
+                        int sum_col = 0;
+                        for (int r = 0; r < k; r++) {
+                            sum_col += grid[i + r][j + c];
+                        }
+                        if (sum_col != first_row) {
+                            isMagic = false;
+                            break;
+                        }
+                    }
+
+                    int left_diag = 0;
+                    for (int l = 0; l < k && isMagic; l++) {
+                        left_diag += grid[i + l][j + l];
+                    }
+                    if (left_diag != first_row) {
+                        isMagic = false;
+                    }
+
+                    int right_diag = 0;
+                    for (int l = 0; l < k && isMagic; l++) {
+                        right_diag += grid[i + l][j + k - 1 - l];
+                    }
+                    if (right_diag != first_row) isMagic = false;
+
+                    if (isMagic) {
+                        ans = Math.max(ans, k);
+                        return ans;
+                    } 
+                }
+            }
+        }
+
+        return ans;
+    }
+}
