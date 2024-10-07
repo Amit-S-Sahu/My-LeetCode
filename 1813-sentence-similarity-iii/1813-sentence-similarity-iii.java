d941ed74d0b5c5242c60be9caa38af609ffb3118
Amit S Sahu <Amit.123854@stu.upes.ac.in>
2024-10-07 14:08:17 +0530

    Time: 1 ms (96.41%), Space: 41.7 MB (39.04%) - LeetHub

--- 1813-sentence-similarity-iii/1813-sentence-similarity-iii.java
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.java
@@ -1,1 +1,21 @@
-        // Check Case... FOR SWITCHING
+class Solution {
+    public boolean areSentencesSimilar(String sentence1, String sentence2) {
+        String[] word1 = sentence1.split(" ");
+        String[] word2 = sentence2.split(" ");
+
+        if (word1.length > word2.length) {
+            return areSentencesSimilar(sentence2, sentence1);
+        }
+
+        int i = 0;
+        while (i < word1.length && word1[i].equals(word2[i])) {
+            i++;
+        }
+
+        while (i < word1.length && word1[i].equals(word2[i + word2.length - word1.length])) {
+            i++;
+        }
+
+        return i == word1.length;
+    }
+}
