9df1fd285f79c1efa2dd934f06beb753699a5508
Amit S Sahu <Amit.123854@stu.upes.ac.in>
2024-10-10 19:44:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxWidthRamp(int[] nums) {
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] <= nums[j]) max = Math.max(max, j - i);
+            }
+        }
+        return max;
+    }
+}
