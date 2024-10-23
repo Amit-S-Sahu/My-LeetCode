db245988bb097ed10ea6c3cb8b9aca6477377566
Amit S Sahu <Amit.123854@stu.upes.ac.in>
2024-10-23 13:58:52 +0530

    Time: 0 ms (100%), Space: 41.9 MB (88.03%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image-brute_force.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int res[][] = new int[n][n];
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                res[j][n - 1 - i] = matrix[i][j];
+            }
+        }
+        
+        for(int i= 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                matrix[i][j] = res[i][j];
+            }
+        }
+        
+    }
+}
