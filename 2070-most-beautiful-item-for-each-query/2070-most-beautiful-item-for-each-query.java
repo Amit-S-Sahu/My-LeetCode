2fc6a4409b2bd6a6b71d6b1886f76a0d5cb990da
Amit S Sahu <Amit.123854@stu.upes.ac.in>
2024-11-12 22:48:40 +0530

    Time: 34 ms (98.55%), Space: 81.2 MB (56.21%) - LeetHub

--- 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.java
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.java
@@ -1,33 +1,20 @@
 class Solution {
     public int[] maximumBeauty(int[][] items, int[] queries) {
-        Map<Integer, Integer> map = new HashMap<>();
+        Arrays.sort(items, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
 
-        for (int i = 0; i < items.length; i++) {
-            map.put(items[i][0], Math.max(map.getOrDefault(items[i][0], 0), items[i][1]));
+        int curr = 0;
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int item[] : items) {
+            if (item[1] <= curr) continue;
+            curr = item[1];
+            map.put(item[0], curr);
         }
-        
-        TreeMap<Integer, Integer> sorted = new TreeMap<>();
 
-        for (int key : map.keySet()) sorted.put(key, map.get(key));
-
-        int prev = 0, c = 0;
-        int arr[] = new int[sorted.size()];
-        int res[] = new int[queries.length];
-
-        for (int key : sorted.keySet()) {
-            sorted.put(key, Math.max(sorted.get(key), prev));
-            prev = sorted.get(key);
-            arr[c++] = key;
-        }
-        
+        int ans[] = new int[queries.length];
         for (int i = 0; i < queries.length; i++) {
-            Integer val = sorted.get(queries[i]);
-            if (val == null) {
-                Integer higherKey = sorted.lowerKey(queries[i]);
-                val = higherKey == null ? 0 : sorted.get(higherKey);
-            }
-            res[i] = val;
+            Map.Entry<Integer, Integer> entry = map.floorEntry(queries[i]);
+            if (entry != null) ans[i] = entry.getValue();   
         }
-        return res;
+        return ans;
     }
 }
