70eefa11dccf0e0d14c00989deb464107247ecf3
Amit S Sahu <Amit.123854@stu.upes.ac.in>
2025-02-19 21:49:07 +0530

    Time: 1 ms (99.09%), Space: 41.4 MB (86.1%) - LeetHub

---
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -0,0 +1,17 @@
+class Solution {
+    int n2;
+    public String getHappyString(int n, int k) {
+        n2 = n;
+        return dfs(new StringBuilder(), n, k);
+    }
+    public String dfs(StringBuilder prefix, int n, int k){
+        if (n == 0) return prefix.toString();
+        for (char c = 'a'; c <= 'c'; c++) {
+            if (prefix.length() > 0 && c == prefix.charAt(prefix.length() - 1)) continue;
+            int count = (int)Math.pow(2, n2 - prefix.length() - 1);
+            if (count >= k) return dfs(prefix.append(c), n - 1, k);
+            else k -= count;
+        }
+        return "";
+    }
+}
