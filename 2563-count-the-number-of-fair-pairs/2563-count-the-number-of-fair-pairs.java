1caa7efb653d768a0826fb5c4a8fbc753b64470f
Amit S Sahu <Amit.123854@stu.upes.ac.in>
2025-04-19 23:57:11 +0530

    Time: 24 ms (78.66%), Space: 57.3 MB (56.8%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
@@ -1,31 +1,17 @@
-class Solution {
-    public long countFairPairs(int[] nums, int lower, int upper) {
-        Arrays.sort(nums);
-        long ans = 0;
-
-        for (int left = 0; left < nums.length - 1; left++) {
-            int start = left + 1;
-            int end = nums.length - 1;
-
-            while (start <= end) {
-                int mid = start + (end - start) / 2;
-                if (nums[left] + nums[mid] >= lower) end = mid - 1;
-                else start = mid + 1;
+class Solution{
+    private long countAtLeast(int[] nums, long comp) {
+        long ans = 0; int i = 0, j = nums.length - 1;
+        while (i < j) {
+            if (nums[i] + nums[j] >= comp) {
+                ans += j - i;
+                j--;
             }
-            int low = start;
-
-            start = left + 1;
-            end = nums.length - 1;
-
-            while (start <= end) {
-                int mid = start + (end - start) / 2;
-                if (nums[left] + nums[mid] <= upper) start = mid + 1;
-                else end = mid - 1;
-            }
-            int high = end;
-
-            if (low <= high) ans += (high - low + 1);
+            else i++;
         }
         return ans;
+    }
+    public long countFairPairs(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+        return countAtLeast(nums, lower) - countAtLeast(nums, upper+1);
     }
 }
