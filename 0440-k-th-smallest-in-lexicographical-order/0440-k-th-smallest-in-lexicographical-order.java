aedfe26fcf16fd7518979452e8ba9ae5c4e0596f
Amit S Sahu <Amit.123854@stu.upes.ac.in>
2025-06-09 13:07:57 +0530

    Time: 0 ms (100%), Space: 40.5 MB (50.23%) - LeetHub

--- 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.java
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.java
@@ -1,27 +1,27 @@
 class Solution {
-    private int getNum(long num1, long num2, long n) {
-        int gap = 0;
-        while (num1 <= n) {
-            gap += Math.min(n + 1, num2) - num1;
-            num1 *= 10;
-            num2 *= 10;
-        }
-        return gap;
+    public int findKthNumber(int n, int K) {
+        long k = K;
+        int ans = solve(0, n, k);
+        return ans;
     }
 
-    public int findKthNumber(int n, int k) {
-        long num = 1;
-        int i = 1; 
-        while (i < k) {
-            int req = getNum(num, num + 1, n);
-            if (i + req <= k) {
-                i += req;
-                num++;
-            } else {
-                i++;
-                num *= 10;
-            }
+    private int solve(long current, long n, long k) {
+        if(k == 0) return (int) (current / 10);
+
+        for (long i = Math.max(current, 1); i < current + 10; i++) {
+            long count = numOfChildren(i, i + 1, n);
+            if (count >= k) return solve(i * 10, n, k - 1);
+            k-= count;
         }
-        return (int)num;
+
+        return -1;
+    }
+
+    private long numOfChildren(long current, long neighbour, long n) {
+        if (neighbour > n) {
+            if (current > n) return 0;
+            return n - current + 1;
+        }
+        return neighbour - current + numOfChildren(current * 10, neighbour * 10, n);
     }
 }
