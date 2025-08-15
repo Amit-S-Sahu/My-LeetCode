b2bdc28124aaed65db1e3988ff035f54785ba128
Amit S Sahu <Amit.123854@stu.upes.ac.in>
2025-08-15 08:51:19 +0530

    Time: 0 ms (100%), Space: 41 MB (45.54%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,9 +1,7 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if (n == 0) return false;
-        while (n % 4 == 0) {
-            n /= 4;
-        }
-        return n == 1;
+        if (n <= 0) return false;
+        if ((n & (n - 1)) == 0 && Integer.bitCount(n - 1) % 2 == 0) return true;
+        return false;
     }
 }
