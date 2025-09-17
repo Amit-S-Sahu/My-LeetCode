f09475305ad069fe0d55c5babcca3d9a2282a1ce
Amit S Sahu <Amit.123854@stu.upes.ac.in>
2025-09-17 16:48:59 +0530

    Time: 1 ms (61.24%), Space: 46.4 MB (60.31%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,21 +1,20 @@
 class Solution {
     public int trap(int[] height) {
         int n = height.length;
-        if (n <= 2) return 0;
         int leftMax[] = new int[n];
         int rightMax[] = new int[n];
+        leftMax[0] = 0;
+        rightMax[n - 1] = 0;
+        for (int i = 1; i < n; i++) leftMax[i] = Math.max(leftMax[i - 1], height[i - 1]);
+        for (int i = n - 2; i >= 0; i--) rightMax[i] = Math.max(rightMax[i + 1], height[i + 1]);
 
-        leftMax[0] = height[0];
-        for (int i = 1; i < height.length; i++) leftMax[i] = Math.max(height[i], leftMax[i - 1]);
-
-        rightMax[height.length - 1] = height[height.length - 1];
-        for (int i = height.length - 2; i >= 0; i--) rightMax[i] = Math.max(height[i], rightMax[i + 1]);
-
-        int trappedWater = 0;
-        for (int i = 0; i < height.length; i++) {
-            int waterLevel = Math.min(leftMax[i], rightMax[i]);
-            trappedWater += waterLevel - height[i];
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int min = Math.min(leftMax[i], rightMax[i]);
+            int water = min - height[i];
+            if (water > 0) ans += water;
         }
-        return trappedWater;
+
+        return ans;
     }
 }
