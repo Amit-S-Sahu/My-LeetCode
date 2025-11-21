2c361737d8e2b58cfeeeafc330c62d85649a5a7a
Amit S Sahu <Amit.123854@stu.upes.ac.in>
2025-11-21 09:06:49 +0530

    Time: 220 ms (20.67%), Space: 60.3 MB (5.17%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
@@ -1,23 +1,21 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-        HashMap<Character, ArrayList<Integer>> map = new HashMap<>();
+        Map<Character, ArrayList<Integer>> map = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
             map.putIfAbsent(s.charAt(i), new ArrayList<>());
             map.get(s.charAt(i)).add(i);
         }
 
         int ans = 0;
-        for (HashMap.Entry<Character, ArrayList<Integer>> entry : map.entrySet()) {
+        for (Map.Entry<Character, ArrayList<Integer>> entry : map.entrySet()) {
             ArrayList<Integer> indices = entry.getValue();
             int start = indices.get(0);
             int end = indices.get(indices.size() - 1);
 
             if (end - start <= 1) continue;
 
-            HashSet<Character> seen = new HashSet<>();
-            for (int i = start + 1; i < end; i++) {
-                seen.add(s.charAt(i));
-            }
+            Set<Character> seen = new HashSet<>();
+            for (int i = start + 1; i < end; i++) seen.add(s.charAt(i));
             ans += seen.size();
         }
 
