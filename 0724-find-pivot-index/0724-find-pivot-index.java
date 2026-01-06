556f038806bc6d659413ac89ce15a82bf5435de7
Amit S Sahu <Amit.123854@stu.upes.ac.in>
2026-01-06 23:24:39 +0530

    Time: 1 ms (97.9%), Space: 47.7 MB (10.08%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int total = 0;
+        for (int num : nums) total += num;
+
+        int left = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (left == total - left - nums[i]) return i;
+            left += nums[i];
+        }
+        return -1;
+    }
+}
