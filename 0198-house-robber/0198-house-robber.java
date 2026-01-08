04603c646f25a0f4c6f30ad34d68839feba366cc
Amit S Sahu <Amit.123854@stu.upes.ac.in>
2026-01-08 13:08:10 +0530

    Time: 0 ms (100%), Space: 43.1 MB (7.18%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,11 +1,19 @@
 class Solution {
+    int dp[];
+    private int helper(int nums[], int n) {
+        if(dp[n] != -1) return dp[n];
+        dp[n] = Math.max(helper(nums, n - 2) + nums[n - 1], helper(nums, n - 1));
+        return dp[n];
+    }
+
     public int rob(int[] nums) {
-        int dp[] = new int[nums.length + 1];
+        int n = nums.length;
+        if (n < 2) return nums[0];
+        dp = new int[n + 1];
+        Arrays.fill(dp, - 1);
         dp[0] = 0;
         dp[1] = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            dp[i + 1] = Math.max(dp[i], dp[i - 1] + nums[i]);
-        }
-        return dp[nums.length];
+        dp[2] = Math.max(nums[0], nums[1]);
+        return Math.max(helper(nums, n), helper(nums, n - 1));
     }
 }
