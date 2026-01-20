732269ef552319ff1a859060d6d2adebf64a6379
Amit S Sahu <Amit.123854@stu.upes.ac.in>
2026-01-20 16:38:41 +0530

    Time: 79 ms (13.93%), Space: 54 MB (99.85%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -4,40 +4,49 @@ class Solution {
         mergesort(nums, 0, nums.length-1);
         return count;
     }
-    private void mergesort(int nums[], int left, int right) {
-        if (left >= right) return;
-        
-        int mid = left + (right - left) / 2;
+    public void mergesort(int nums[], int left, int right) {
+        if (left>=right) {
+            return;
+        }
+        int mid = (left+right)/2;
         mergesort(nums, left, mid);
-        mergesort(nums, mid + 1, right);
+        mergesort(nums, mid+1, right);
         countpairs(nums, left, mid, right);
         merge(nums, left, mid, right);
     }
-
-    private void merge(int nums[], int left, int mid, int right) {
-        int i1 = left;
-        int i2 = mid+1;
+    public void merge(int nums[], int left, int mid, int right) {
+        int idx1 = left;
+        int idx2 = mid+1;
         ArrayList<Integer> temp = new ArrayList<>();
-        while (i1 <= mid && i2 <= right) {
-            if (nums[i1] <= nums[i2]) temp.add(nums[i1++]);
-            else temp.add(nums[i2++]);
+        while (idx1<=mid && idx2<=right) {
+            if (nums[idx1] <= nums[idx2]) {
+                temp.add(nums[idx1]);
+                idx1++;
+            }
+            else {
+                temp.add(nums[idx2]);
+                idx2++;
+            }
         }
-
-        while (i1 <= mid) temp.add(nums[i1++]);
-        while (i2 <= right) temp.add(nums[i2++]);
-
-        for (int i = left; i <= right; i++) {
+        while (idx1<=mid) {
+            temp.add(nums[idx1]);
+            idx1++;
+        }
+        while (idx2<=right) {
+            temp.add(nums[idx2]);
+            idx2++;
+        }
+        for (int i=left; i<=right; i++) {
             nums[i] = temp.get(i-left);
         }
     }
-
-    private void countpairs(int nums[], int low, int mid, int high) {
-        int right = mid + 1;
-        for (int i = low; i <= mid; i++) {
-            while(right <= high && (long)nums[i] > (long)2 * (long)nums[right]) {
+    public void countpairs(int nums[], int low, int mid, int high) {
+        int right = mid+1;
+        for (int i=low; i<=mid; i++) {
+            while(right<=high && (long)nums[i]>(long)2*(long)nums[right]) {
                 right++;
             }
-            count += (right - (mid + 1));
+            count += (right-(mid+1));
         }
     }
 }
