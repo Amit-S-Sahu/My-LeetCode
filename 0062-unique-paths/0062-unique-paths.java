f3d3c992088e5c5f25e8c9c7febccb6948b5e5a8
Amit S Sahu <Amit.123854@stu.upes.ac.in>
2026-01-24 14:34:08 +0530

    Time: 1 ms (14.9%), Space: 42.4 MB (19.75%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,25 +1,14 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int dp[][] = new int[m][n];
+        for (int i = 0; i < m; i++) Arrays.fill(dp[i], -1);
+        for (int i = 0; i < m; i++) dp[i][0] = 1;
+        for (int i = 0; i < n; i++) dp[0][i] = 1;
 
-        for(int i = 0; i<dp.length;i++){
-            for(int j = 0; j < dp[0].length; j++){
-                dp[i][j] = -1;
-            }
-        }
-        return helper(0, 0, m, n, dp);
-    }
-
-    static int helper(int row, int col, int m, int n, int[][] dp){
-        if(row==m-1 && col==n-1) return 1;
-        if(dp[row][col]!=-1) return dp[row][col];
-
-        if(row == m - 1){
-            return dp[row][col] = helper(row, col+1, m, n, dp);
-        }else if(col == n - 1){
-           return dp[row][col] = helper(row+1, col, m, n, dp);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
         }
 
-        return dp[row][col] = helper(row+1, col, m, n, dp) + helper(row, col+1, m, n, dp);
+        return dp[m - 1][n - 1];
     }
 }
