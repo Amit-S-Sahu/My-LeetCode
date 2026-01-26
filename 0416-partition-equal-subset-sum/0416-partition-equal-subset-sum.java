dfeabfbdd7afaee7d488ff79e969e9ddf61a9f2a
Amit S Sahu <Amit.123854@stu.upes.ac.in>
2026-01-26 09:31:46 +0530

    Time: 39 ms (66.32%), Space: 75.7 MB (20%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,19 +1,27 @@
 class Solution {
-    public boolean canPartition(int[] nums) {
-        int total = 0;
-        for (int num : nums) total += num;
+    private boolean helper(int nums[], Boolean dp[][], int n, int sum) {
+        if (sum == 0) return true;
+        if (n <= 0) return false;
+        if (dp[n][sum] != null) return dp[n][sum];
 
-        if (total % 2 != 0) return false;
-        
-        int target = total / 2;
-        boolean dp[] = new boolean[target + 1];
-        dp[0] = true;
-        for (int num : nums) {
-            for (int curr = target; curr >= num; curr--) {
-                dp[curr] = dp[curr] || dp[curr - num];
-                if (dp[target]) return true;
-            }
+        if (nums[n - 1] <= sum) {
+            dp[n][sum] = helper(nums, dp, n - 1, sum) || helper(nums, dp, n - 1, sum - nums[n - 1]);
         }
-        return dp[target];
+        else dp[n][sum] = helper(nums, dp, n - 1, sum);
+
+        return dp[n][sum];
+    }
+
+    public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        for (int num : nums) sum += num;
+        if ((sum & 1) == 1) return false;
+        sum /= 2;
+        Boolean dp[][] = new Boolean[n + 1][sum + 1];
+        for (int i = 0; i <= n; i++) dp[i][0] = true;
+        for (int i = 1; i <= sum; i++) dp[0][i] = false;
+        
+        return helper(nums, dp, n, sum);
     }
 }
