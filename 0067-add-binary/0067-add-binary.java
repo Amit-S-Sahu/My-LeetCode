5c87d81c4064c4698facb18ee008f53051183fc0
Amit S Sahu <Amit.123854@stu.upes.ac.in>
2026-02-15 16:20:57 +0530

    Time: 1 ms (99.65%), Space: 42.1 MB (99.99%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -7,12 +7,9 @@ class Solution
     int i = a.length() - 1;
     int j = b.length() - 1;
 
-    while (i >= 0 || j >= 0 || carry == 1) 
-    {
-      if(i >= 0)
-        carry += a.charAt(i--) - '0';
-      if(j >= 0)
-        carry += b.charAt(j--) - '0';
+    while (i >= 0 || j >= 0 || carry == 1) {
+      if(i >= 0) carry += a.charAt(i--) - '0';
+      if(j >= 0) carry += b.charAt(j--) - '0';
       sb.append(carry % 2);
       carry /= 2;
     }
