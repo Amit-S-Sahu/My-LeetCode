e8a400da8de71ae2b4e9068a3891a389189f597c
Amit S Sahu <Amit.123854@stu.upes.ac.in>
2026-02-20 19:09:30 +0530

    Time: 6 ms (2.84%), Space: 43.1 MB (89.57%) - LeetHub

---
+++ 0761-special-binary-string/0761-special-binary-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String makeLargestSpecial(String s) {
+        int count =0;
+        List<String> list = new LinkedList<>();
+        int j = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') count++;
+            else count--;
+            if (count == 0) {
+                list.add('1' + makeLargestSpecial(s.substring(j + 1, i)) + '0');
+                j = i + 1;
+            }
+        }
+        Collections.sort(list, Collections.reverseOrder());
+        return String.join("", list);
+    }
+}
