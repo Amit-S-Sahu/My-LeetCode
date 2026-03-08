cb1df1124d44842a008c5f3295786b91b689ba1c
Amit S Sahu <Amit.123854@stu.upes.ac.in>
2026-03-08 09:21:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.java
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -1,18 +1,9 @@
 class Solution {
-    HashSet<Integer> set;
     public String findDifferentBinaryString(String[] nums) {
-        int n = nums.length;
-
-        set = new HashSet<>();
-        for (String num : nums) {
-            set.add(Integer.parseInt(num, 2));
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < nums.length; i++){
+            ans.append(nums[i].charAt(i) == '0' ? '1' :'0');
         }
-
-        int total = (int)Math.pow(2, n);
-        for (Integer i = 0; i < total; i++) {
-            if (!set.contains(i)) return String.format("%" + n + "s", Integer.toBinaryString(i)).replace(' ', '0');
-        }
-
-        return "";
+        return ans.toString();
     }
 }
