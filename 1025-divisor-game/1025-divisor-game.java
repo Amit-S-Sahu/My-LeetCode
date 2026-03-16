4e894a02552b2da9383b9a8efe3e496fd5228ded
Amit S Sahu <Amit.123854@stu.upes.ac.in>
2026-03-16 13:29:26 +0530

    Time: 2 ms (6.32%), Space: 42.1 MB (68.4%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.java
+++ 1025-divisor-game/1025-divisor-game.java
@@ -1,5 +1,11 @@
 class Solution {
     public boolean divisorGame(int n) {
-        return (n & 1) == 0;
+        boolean dp[] = new boolean[n + 1];
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j * j < n; j++) {
+                if (i % j == 0) dp[i] = dp[i] | !dp[i - j];
+            }
+        }
+        return dp[n];
     }
 }
