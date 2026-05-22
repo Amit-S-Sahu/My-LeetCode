c63bb07bfd86cdd8b03c34bd7a233560d895646f
Amit S Sahu <Amit.123854@stu.upes.ac.in>
2026-05-22 11:54:56 +0530

    Time: 84 ms (1.41%), Space: 43.9 MB (45.24%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -7,20 +7,12 @@ class Solution {
             if (nums[mid] == target) return mid;
 
             if (nums[left] <= nums[mid]) {
-                if(nums[left] <= target && target <= nums[mid]) {
-                    right = mid - 1;
-                }
-                else {
-                    left = mid + 1;
-                }
+                if(nums[left] <= target && target <= nums[mid]) right = mid - 1;
+                else left = mid + 1;
             }
             else {
-                if(nums[mid] <= target && target <= nums[right]) {
-                    left = mid + 1;
-                }
-                else {
-                    right = mid - 1;
-                }
+                if(nums[mid] <= target && target <= nums[right]) left = mid + 1;
+                else right = mid - 1;
             }
         }
         return -1;
